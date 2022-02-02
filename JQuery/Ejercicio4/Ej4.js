023332ad9393f56d278f5ce6674044b05682d868
jorge1019 <jorge10.lominchar@gmail.com>
2022-02-02 13:19:49 +0100

    Clase 02/02/2022

--- JQuery/Ejercicio4/Ej4.js
+++ JQuery/Ejercicio4/Ej4.js
@@ -1,22 +1,32 @@
 $(document).ready(function(){
-    $(".cuadrado").on({
+    $(".cuadrado").on(
 
-"click":function(){
+"click",function(){
     $("body").css("background-color",$(this).css("background-color"));
   
-},
-"mousemove":function(evento){
-$(".tip")
-.text($(this).data("nombre"))
-.css({
-"display":"block"
+}),
+$(".cuadrado").on("mouseenter",function(evento){
+$(".tip").css({
+"display":"block",
 
 
+"background-color":"lightgreen",
+"width":150,
+"height":50,
+"text-align":"center",
+"position":"absolute",
+"visibility":"visible",
+"line-height":3,
+"border-radius":5,
+"left":evento.pageX,
+"top":evento.pageY
 
 
 })
-}
+$(".tip").text($(this).data("nombre"))
+
+})
 
 
-    })
+   
 })
